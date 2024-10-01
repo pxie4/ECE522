36e3babec9a8c867e10bd9baaa444e50c6f69172
pxie4 <pxie4@node0.px4.pfscheck-pg0.utah.cloudlab.us>
2024-09-30 19:40:10 -0600

    done

--- mp2/hugepage/perf-all.sh
+++ mp2/hugepage/perf-all.sh
@@ -5,15 +5,15 @@ if [ $# -ne 1 ]; then
 	exit
 fi
 
-home_dir=`echo $HOME`
+home_dir=`echo $HOME/ECE522/mp2`
 
 for bench in canneal streamcluster
 do
 	TS=`date +%Y-%m-%d_%H-%M-%S`
 
-	mkdir -p $home_dir/ECE522/mp2/hugepage/results/$bench
-	sudo perf stat -x, -o $home_dir/ECE522/mp2/hugepage/results/$bench/$1.$bench.$TS.csv --cpu=0 --append -e dTLB-load-misses,iTLB-load-misses,cache-misses $home_dir/ECE522/hugepage/parsec-3.0/bin/parsecmgmt -a run -p $bench -i native -n 1 -c gcc-pthreads &>> $home_dir/ECE522/mp2/hugepage/results/$bench/$1.$bench.$TS.log
-	if [ $? -ne 0 ]; 
+	mkdir -p $home_dir/hugepage/results/$bench
+	sudo perf stat -x, -o $home_dir/hugepage/results/$bench/$1.$bench.$TS.csv --cpu=0 --append -e dTLB-load-misses,iTLB-load-misses,cache-misses $home_dir/hugepage/parsec-3.0/bin/parsecmgmt -a run -p $bench -i native -n 1 -c gcc-pthreads &>> $home_dir/hugepage/results/$bench/$1.$bench.$TS.log
+	if [ $? -ne 0 ]; then
     	echo "Error running benchmark $bench"
 	fi	
 done
@@ -21,10 +21,10 @@ done
 for bench in fmm
 do
 	TS=`date +%Y-%m-%d_%H-%M-%S`
-
-	mkdir -p $home_dir/ECE522/mp2/hugepage/results/$bench
-	sudo perf stat -x, -o $home_dir/ECE522/mp2/hugepage/results/$bench/$1.$bench.$TS.csv --cpu=0 --append -e dTLB-load-misses,iTLB-load-misses,cache-misses $home_dir/ECE522/hugepage/parsec-3.0/bin/parsecmgmt -a run -p splash2x.$bench -i native -n 1 -c gcc-pthreads &>> $home_dir/ECE522/mp2/hugepage/results/$bench/$1.$bench.$TS.log 
+	mkdir -p $home_dir/hugepage/results/$bench
+	sudo perf stat -x, -o $home_dir/hugepage/results/$bench/$1.$bench.$TS.csv --cpu=0 --append -e dTLB-load-misses,iTLB-load-misses,cache-misses $home_dir/hugepage/parsec-3.0/bin/parsecmgmt -a run -p splash2x.$bench -i native -n 1 -c gcc-pthreads &>> $home_dir/hugepage/results/$bench/$1.$bench.$TS.log 
 	if [ $? -ne 0 ]; then
     	echo "Error running benchmark $bench"
 	fi	
 done
+
